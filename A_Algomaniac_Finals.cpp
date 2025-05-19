8d26880579d00a7a14e66a90b937c042e27b5287
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-19 22:12:04 +0600

    Add files via upload

---
+++ A_Algomaniac_Finals.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int X;
+    cin >> X; 
+    if (X == 17) 
+    {
+        cout << "YAY\n";  
+    } 
+    else 
+    {
+        cout << "NO\n";  
+    }
+    return 0;
+}
